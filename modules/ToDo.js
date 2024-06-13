7da6eb788baf27e57e41a9a9e2341a31d18cbcd8
GKVS <itsergey1001@gmail.com>
2024-06-13 19:58:34 +0500

    move workTask to ToDo class

--- modules/ToDo.js
+++ modules/ToDo.js
@@ -2,7 +2,6 @@
 export {ToDo as default};
 
 import workProjects from "./ToDo/workProjects.js";
-import workTasks from "./ToDo/workTasks.js";
 import workTags from "./ToDo/workTags.js";
 
 /**
@@ -14,7 +13,7 @@ class ToDo {
     /**
      * Объект для хранения задач
      *
-     * @type {Map}
+     * @type {Object}
      * @private
      * */
     _taskList = {};
@@ -38,6 +37,40 @@ class ToDo {
     constructor(name) {
         this.name = name;
     }
+
+    /**
+     * Добавляет задачу в список задач
+     * @param {object} task - Объект задачи ( Экземпляр класса Task )
+     * @param {String} idTask - Уникальный идентификатор
+     * */
+    addTaskList(task, idTask = undefined) {}
+
+    /**
+     * Удаляет задачу из списка задач
+     * @param {string} idTask - Идентификатор задачи
+     * */
+    deleteTask(idTask) {}
+
+    /**
+     * Изменятет парамметры задачи
+     * @param {string} idTask - Идентификатор задачи
+     * @param {object} params - Объект с парамметрами задачи
+     * */
+    editTask(idTask, params) {}
+
+    /**
+     * Создате задачу
+     * @param {string} name - Имя задачи
+     * @param {object} params - Парраметры задачи
+     * */
+    createTask(name, params) {}
+
+    /**
+     * Возвращает объект задачи
+     * @param {string} idTask - Идентификатор задачи
+     * @return {object} - Возращает объект задачи
+     * */
+    getTask(idTask) {}
 }
 
 Object.assign(ToDo.prototype, workTasks, workProjects, workTags)
